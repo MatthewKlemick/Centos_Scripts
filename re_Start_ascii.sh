ed41ad10a82f16989b9914b8df588a61124893d8
MatthewKlemick <73146834+MatthewKlemick@users.noreply.github.com>
2022-04-07 08:26:51 +1200

    Create re_Start_ascii.sh

---
+++ re_Start_ascii.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Usage: curl  | bash
+
+echo "##########################################"
+echo "#                                        #"
+echo "#                  ...                   #"
+echo "#                 .oKKo.                 #"
+echo "#                 .xWMWx.                #"
+echo "#               .. .dXNWk'               #"
+echo "#              ;Ok' .'oNWO,              #"
+echo "#             :KMW0,  .cXMK:             #"
+echo "#            cXMNNMK:.  :KMXc            #"
+echo "#          .oNM0:cKMXOc  ;0WNl.          #"
+echo "#         .xWWO'  ,0WMNo. 'OWNd.         #"
+echo "#        .cOOd.    'dO0k:  .dOOc         #"
+echo "#                                        #"
+echo "##########################################"
