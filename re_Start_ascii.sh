6a8bb0c7d62db239f94cf441cb371b00fb4304e7
MatthewKlemick <73146834+MatthewKlemick@users.noreply.github.com>
2022-04-07 16:03:23 +1200

    Update re_Start_ascii.sh
    Added colors

--- re_Start_ascii.sh
+++ re_Start_ascii.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 # Usage: curl https://raw.githubusercontent.com/MatthewKlemick/Centos_Scripts/main/re_Start_ascii.sh | bash
+# Colors
+Color_Off='\033[0m'
+Blue='\033[0;34m'
+On_Purple='\033[45m'
+White='\033[0;97m'
 
-echo "##########################################"
-echo "#                                        #"
-echo "#                  ...                   #"
-echo "#                 .oKKo.                 #"
-echo "#                 .xWMWx.                #"
-echo "#               .. .dXNWk'               #"
-echo "#              ;Ok' .'oNWO,              #"
-echo "#             :KMW0,  .cXMK:             #"
-echo "#            cXMNNMK:.  :KMXc            #"
-echo "#          .oNM0:cKMXOc  ;0WNl.          #"
-echo "#         .xWWO'  ,0WMNo. 'OWNd.         #"
-echo "#        .cOOd.    'dO0k:  .dOOc         #"
-echo "#                                        #"
-echo "##########################################"
+echo -e "${Blue}${On_Purple}##########################################"
+echo -e "#                                        #"
+echo -e "#                  ...                   #"
+echo -e "#                 .oKKo.                 #"
+echo -e "#                 .xWMWx.                #"
+echo -e "#               .. .dXNWk'               #"
+echo -e "#              ;Ok' .'oNWO,              #"
+echo -e "#             :KMW0,  .cXMK:             #"
+echo -e "#            cXMNNMK:.  :KMXc            #"
+echo -e "#          .oNM0:cKMXOc  ;0WNl.          #"
+echo -e "#         .xWWO'  ,0WMNo. 'OWNd.         #"
+echo -e "#        .cOOd.    'dO0k:  .dOOc         #"
+echo -e "#                                        #"
+echo -e "##########################################${Color_Off}"
